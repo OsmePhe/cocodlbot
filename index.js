932faf4f133dedd998accd235ed4d934c3a053ee
OsmePhe <omarogoubi@yahoo.fr>
2022-01-31 23:46:09 +0100

    redirect to https

--- index.js
+++ index.js
@@ -139,6 +139,14 @@ async function resultFile(id, res) {
 function responseCallback(err) {
     if(err) console.log("error:", err)
 }
+// if(process.env.NODE_ENV === 'production') {
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https')
+    res.redirect(`https://${req.header('host')}${req.url}`)
+  else
+    next()
+  })
+// }
 app.use(express.static('public'));
 app.get('/home*', function (req, res) {
    res.sendFile(path.join(__dirname, 'public', 'index.html'));
